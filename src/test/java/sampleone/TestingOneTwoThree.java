ac0344b00b1145211591e3e9d544fa9644f65fe5
Yinka <multiyinkus@gmail.com>
2024-08-07 20:42:46 +0100

    New Line added to TestingOneTwoThree

--- src/test/java/sampleone/TestingOneTwoThree.java
+++ src/test/java/sampleone/TestingOneTwoThree.java
@@ -17,7 +17,8 @@ public class TestingOneTwoThree {
 		driver.manage().deleteAllCookies();
 		driver.findElement(By.id("men-floor")).click();
 		driver.findElement(By.xpath("(//span[@class='cSfPh9_']//span[contains(text(),'Clothing')])[2]")).click();
-		System.out.println("This is completed now1");
+		System.out.println("This is completed now");
+		System.err.println("This is the 2nd part");
 		driver.close();
 		
 
